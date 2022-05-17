f09e3da06e9c388bbd19498c4e089c8d12ad560d
tsonobe <sono.tak.st@gmail.com>
2022-05-18 07:06:41 +0900

    Initial Commit

---
+++ imonit/imonitApp.swift
@@ -0,0 +1,20 @@
+//
+//  imonitApp.swift
+//  imonit
+//
+//  Created by 薗部拓人 on 2022/05/18.
+//
+
+import SwiftUI
+
+@main
+struct imonitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
